d38a9ba5d744a9e3409c1ae9034b2ba12fead848
vytautassugintas <vytautas.sugintas@gmail.com>
2019-06-28 00:14:22 +0300

    feat: add MutationObserver

--- demo-saas/index.ts
+++ demo-saas/index.ts
@@ -1,3 +1,25 @@
-const message: string = "hi";
+const onbserverConfig: MutationObserverInit = {
+  attributes: true,
+  childList: true,
+  subtree: true
+};
 
-console.log(message);
+const mutationCallback: MutationCallback = function(
+  mutationsList: MutationRecord[]
+): void {
+  for (const mutation of mutationsList) {
+    console.log(mutation.target.textContent);
+  }
+};
+
+const observer = new MutationObserver(mutationCallback);
+const container: HTMLElement = document.getElementById("container");
+
+let clickCount: number = 0;
+
+const onClickListener = container.addEventListener("click", () => {
+  clickCount = clickCount + 1;
+  container.innerHTML = `${clickCount}`;
+});
+
+observer.observe(container, onbserverConfig);
